285c49d8e0eaa9bb638e984f8ea4158739a7cc46
AmalH <hichri.amal@hotmail.fr>
2018-04-25 04:35:38 +0100

    Service layer

---
+++ Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using Data.Infrastructure;
+using MyFinance.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+
+
+        private static IDatabaseFactory dbf = new DatabaseFactory();
+        private static IUnitOfWork ut = new UnitOfWork(dbf);
+
+        public ProductService()
+           : base(ut)
+        {
+
+
+
+        }
+    }
+
+}
